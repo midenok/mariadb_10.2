79aa7ffb3969f4294c3b7352def32d17da3705ec
Alexey Botchkov <holyfoot@askmonty.org>
2019-06-27 02:19:44 +0400

    MDEV-19275 Provide SQL service to plugins.
    Protocol_local fixed so it can be used now.
    Some Protocol:: methods made virtual so they can adapt.
    as well as net_ok and net_send_error functions.
    execute_sql_string function is exported to the plugins.
    To be changed with the mysql_use_result.

--- sql-common/client.c
+++ sql-common/client.c
@@ -3733,6 +3733,12 @@ static MYSQL_RES * cli_use_result(MYSQL *mysql)
 }
 
 
+my_ulonglong STDCALL mysql_affected_rows(MYSQL *mysql)
+{
+  return mysql->affected_rows;
+}
+
+
 /**************************************************************************
   Return next row of the query results
 **************************************************************************/
