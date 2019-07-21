9941c6a31797ec43dbddfbb588c36eb551849142
Aleksey Midenkov <midenok@gmail.com>
2019-07-21 23:30:49 +0300

    MDEV-17554 Auto-create new partition for system versioned tables with history partitioned by INTERVAL/LIMIT
    Syntax change:
    
    Keyword AUTO_INCREMENT (or just AUTO) enables partition auto-creation.
    
    create or replace table t1 (x int) with system versioning
    partition by system_time interval 1 hour auto_increment;
    
    create or replace table t1 (x int) with system versioning
    partition by system_time limit 1000 auto_increment;
    
    Or with explicit partitions:
    
    create or replace table t1 (x int) with system versioning
    partition by system_time interval 1 hour auto_increment
    (partition p0 history, partition pn current);
    
    When there are VERS_MIN_EMPTY empty partitions left, auto-create N new
    empty partitions.
    
    This scheme must not allow partition
    overflow. I.e. VERS_MIN_EMPTY-fill time must not exceed N-creation
    time. This means that low values for INTERVAL and LIMIT must not be
    allowed for auto-creation. In case when overflow is detected there is
    no need to do anything special: a warning is issued and the user must
    run manual rebuild to redistribute records correctly. This is
    important because automatic creation of partition is fast, without
    forced rebuild by the reason of performance. Auto-creation is
    implemented by fast_alter_partition_table() call from the thread of
    the main DML command.
    
    Current revision implements hard-coded values of 1 for VERS_MIN_EMPTY
    and N. As well as auto-creation threshold VERS_MIN_INTERVAL = 1 hour,
    VERS_MIN_LIMIT = 1000.
    
    The name for newly added partition is chosen as "pX" where X is
    partition number and "p" is hard-coded name prefix. If this name is
    already occupied the X is incremented until the resulting name is free
    to use.

--- sql/sql_partition.cc
+++ sql/sql_partition.cc
@@ -2584,11 +2584,15 @@ char *generate_partition_syntax(THD *thd, partition_info *part_info,
         err+= str.append(ctime, ctime_len);
         err+= str.append('\'');
       }
+      if (vers_info->auto_inc)
+        err+= str.append(STRING_WITH_LEN(" AUTO_INCREMENT"));
     }
-    if (vers_info->limit)
+    else if (vers_info->limit)
     {
       err+= str.append(STRING_WITH_LEN("LIMIT "));
       err+= str.append_ulonglong(vers_info->limit);
+      if (vers_info->auto_inc)
+        err+= str.append(STRING_WITH_LEN(" AUTO_INCREMENT"));
     }
   }
   else if (part_info->part_expr)
@@ -5312,7 +5316,9 @@ that are reorganised.
               now_part= el;
             }
           }
-          if (*fast_alter_table && tab_part_info->vers_info->interval.is_set())
+          if (*fast_alter_table &&
+              !(alter_info->partition_flags & ALTER_PARTITION_AUTO_HIST) &&
+              tab_part_info->vers_info->interval.is_set())
           {
             partition_element *hist_part= tab_part_info->vers_info->hist_part;
             if (hist_part->range_value <= thd->query_start())
@@ -5977,7 +5983,7 @@ err:
                                records are added
 */
 
-static bool mysql_change_partitions(ALTER_PARTITION_PARAM_TYPE *lpt)
+static bool mysql_change_partitions(ALTER_PARTITION_PARAM_TYPE *lpt, bool copy_data)
 {
   char path[FN_REFLEN+1];
   int error;
@@ -5987,7 +5993,7 @@ static bool mysql_change_partitions(ALTER_PARTITION_PARAM_TYPE *lpt)
 
   build_table_filename(path, sizeof(path) - 1, lpt->db.str, lpt->table_name.str, "", 0);
 
-  if(mysql_trans_prepare_alter_copy_data(thd))
+  if(copy_data && mysql_trans_prepare_alter_copy_data(thd))
     DBUG_RETURN(TRUE);
 
   /* TODO: test if bulk_insert would increase the performance */
@@ -6001,7 +6007,9 @@ static bool mysql_change_partitions(ALTER_PARTITION_PARAM_TYPE *lpt)
     file->print_error(error, MYF(error != ER_OUTOFMEMORY ? 0 : ME_FATAL));
   }
 
-  if (mysql_trans_commit_alter_copy_data(thd))
+  DBUG_ASSERT(copy_data || (!lpt->copied && !lpt->deleted));
+
+  if (copy_data && mysql_trans_commit_alter_copy_data(thd))
     error= 1;                                /* The error has been reported */
 
   DBUG_RETURN(MY_TEST(error));
@@ -7069,7 +7077,8 @@ uint fast_alter_partition_table(THD *thd, TABLE *table,
   lpt->pack_frm_len= 0;
 
   if (table->file->alter_table_flags(alter_info->flags) &
-        HA_PARTITION_ONE_PHASE)
+        HA_PARTITION_ONE_PHASE &&
+      !(alter_info->partition_flags & ALTER_PARTITION_AUTO_HIST))
   {
     /*
       In the case where the engine supports one phase online partition
@@ -7111,7 +7120,7 @@ uint fast_alter_partition_table(THD *thd, TABLE *table,
       2) Perform the change within the handler
     */
     if (mysql_write_frm(lpt, WFRM_WRITE_SHADOW) ||
-        mysql_change_partitions(lpt))
+        mysql_change_partitions(lpt, true))
     {
       goto err;
     }
@@ -7216,9 +7225,10 @@ uint fast_alter_partition_table(THD *thd, TABLE *table,
       goto err;
     }
   }
-  else if ((alter_info->partition_flags & ALTER_PARTITION_ADD) &&
+  else if ((alter_info->partition_flags & ALTER_PARTITION_AUTO_HIST) || (
+           (alter_info->partition_flags & ALTER_PARTITION_ADD) &&
            (part_info->part_type == RANGE_PARTITION ||
-            part_info->part_type == LIST_PARTITION))
+            part_info->part_type == LIST_PARTITION)))
   {
     /*
       ADD RANGE/LIST PARTITIONS
@@ -7262,7 +7272,7 @@ uint fast_alter_partition_table(THD *thd, TABLE *table,
         write_log_add_change_partition(lpt) ||
         ERROR_INJECT_CRASH("crash_add_partition_4") ||
         ERROR_INJECT_ERROR("fail_add_partition_4") ||
-        mysql_change_partitions(lpt) ||
+        mysql_change_partitions(lpt, false) ||
         ERROR_INJECT_CRASH("crash_add_partition_5") ||
         ERROR_INJECT_ERROR("fail_add_partition_5") ||
         (close_table_on_failure= FALSE, FALSE) ||
@@ -7358,7 +7368,7 @@ uint fast_alter_partition_table(THD *thd, TABLE *table,
         write_log_add_change_partition(lpt) ||
         ERROR_INJECT_CRASH("crash_change_partition_3") ||
         ERROR_INJECT_ERROR("fail_change_partition_3") ||
-        mysql_change_partitions(lpt) ||
+        mysql_change_partitions(lpt, true) ||
         ERROR_INJECT_CRASH("crash_change_partition_4") ||
         ERROR_INJECT_ERROR("fail_change_partition_4") ||
         wait_while_table_is_used(thd, table, HA_EXTRA_NOT_USED) ||
