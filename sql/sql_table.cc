7c02168194b5fb015fb8682679eb6c5628b9f12b
Aleksey Midenkov <midenok@gmail.com>
2019-12-20 20:04:08 +0300

    innodb.foreign_key fix

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -75,7 +75,8 @@ static int copy_data_between_tables(THD *, TABLE *,TABLE *,
                                     Alter_info::enum_enable_or_disable,
                                     Alter_table_ctx *);
 static int mysql_prepare_create_table(THD *, HA_CREATE_INFO *, Alter_info *,
-                                      uint *, handler *, KEY **, uint *, int);
+                                      uint *, handler *, KEY **, uint *, int,
+                                      const LEX_CSTRING &table_name);
 static uint blob_length_by_type(enum_field_types type);
 static bool fix_constraints_names(THD *thd, List<Virtual_column_info>
                                   *check_constraint_list,
@@ -1826,7 +1827,7 @@ bool mysql_write_frm(ALTER_PARTITION_PARAM_TYPE *lpt, uint flags)
     if (mysql_prepare_create_table(lpt->thd, lpt->create_info, lpt->alter_info,
                                    &lpt->db_options, lpt->table->file,
                                    &lpt->key_info_buffer, &lpt->key_count,
-                                   C_ALTER_TABLE))
+                                   C_ALTER_TABLE, lpt->table_name))
     {
       DBUG_RETURN(TRUE);
     }
@@ -3466,7 +3467,8 @@ static int
 mysql_prepare_create_table(THD *thd, HA_CREATE_INFO *create_info,
                            Alter_info *alter_info, uint *db_options,
                            handler *file, KEY **key_info_buffer,
-                           uint *key_count, int create_table_mode)
+                           uint *key_count, int create_table_mode,
+                           const LEX_CSTRING &table_name)
 {
   Lex_cstring   key_name;
   std::set<Lex_cstring> key_names;
@@ -3809,22 +3811,9 @@ mysql_prepare_create_table(THD *thd, HA_CREATE_INFO *create_info,
       {
         if (key->foreign)
         {
-          List_iterator_fast<Key_part_spec> cols(key->columns);
-          column= cols++;
-          it.rewind();
-          while ((sql_field=it++) &&
-                lex_string_cmp(system_charset_info, &column->field_name,
-                                &sql_field->field_name));
-          if (!sql_field)
-          {
-            my_error(ER_KEY_COLUMN_DOES_NOT_EXITS, MYF(0), column->field_name.str);
-            DBUG_RETURN(TRUE);
-          }
-          if (!(key_name= key->name).str)
-            key_name= make_unique_key_name(thd, sql_field->field_name,
-                                           key_names, true);
-          key->name= key_name;
-          key_names.insert(key_name);
+          if (!key->name.str)
+            key->name= make_unique_key_name(thd, table_name, key_names, true);
+          key_names.insert(key->name);
         }
         key=key_iterator++;
       }
@@ -4199,8 +4188,9 @@ mysql_prepare_create_table(THD *thd, HA_CREATE_INFO *create_info,
           }
 	}
 	else if (!(key_name= key->name).str)
-	  key_name= make_unique_key_name(thd, sql_field->field_name, key_names,
-                                         key->foreign);
+	  key_name= key->foreign ?
+            make_unique_key_name(thd, table_name, key_names, true) :
+            make_unique_key_name(thd, sql_field->field_name, key_names, false);
 	if (key_names.find(key_name) != key_names.end())
 	{
 	  my_error(ER_DUP_KEYNAME, MYF(0), key_name.str);
@@ -4830,7 +4820,8 @@ handler *mysql_create_frm_image(THD *thd, const LEX_CSTRING &db,
   }
 
   if (mysql_prepare_create_table(thd, create_info, alter_info, &db_options,
-                                 file, key_info, key_count, create_table_mode))
+                                 file, key_info, key_count, create_table_mode,
+                                 table_name))
     goto err;
   create_info->table_options=db_options;
 
@@ -7320,7 +7311,8 @@ bool mysql_compare_tables(TABLE *table,
                            C_ORDINARY_CREATE : C_ALTER_TABLE;
   if (mysql_prepare_create_table(thd, create_info, &tmp_alter_info,
                                  &db_options, table->file, &key_info_buffer,
-                                 &key_count, create_table_mode))
+                                 &key_count, create_table_mode,
+                                 table->s->table_name))
     DBUG_RETURN(1);
 
   /* Some very basic checks. */
