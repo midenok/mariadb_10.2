37a3dc4da88c48bb86b00610a89d9ee4f101200f
Aleksey Midenkov <midenok@gmail.com>
2020-01-24 20:55:54 +0300

    Recover dict0crea

--- storage/innobase/handler/handler0alter.cc
+++ storage/innobase/handler/handler0alter.cc
@@ -8664,7 +8664,7 @@ func_exit:
 @param foreign_id Foreign key constraint identifier
 @retval true Failure
 @retval false Success */
-MY_ATTRIBUTE((nonnull, warn_unused_result))
+static MY_ATTRIBUTE((nonnull, warn_unused_result))
 bool
 innobase_drop_foreign_try(
 /*======================*/
@@ -8674,6 +8674,7 @@ innobase_drop_foreign_try(
 {
 	DBUG_ENTER("innobase_drop_foreign_try");
 
+	DBUG_ASSERT(trx_get_dict_operation(trx) == TRX_DICT_OP_INDEX);
 	ut_ad(trx->dict_operation_lock_mode == RW_X_LATCH);
 	ut_d(dict_sys.assert_locked());
 
@@ -8692,50 +8693,6 @@ innobase_drop_foreign_try(
 	pars_info_add_str_literal(info, "id", foreign_id);
 
 	trx->op_info = "dropping foreign key constraint from dictionary";
-	error = que_eval_sql(info, sql, FALSE, trx);
-	trx->op_info = "";
-
-	DBUG_EXECUTE_IF("ib_drop_foreign_error",
-			error = DB_OUT_OF_FILE_SPACE;);
-
-	if (error != DB_SUCCESS) {
-		my_error_innodb(error, table_name, 0);
-		trx->error_state = DB_SUCCESS;
-		DBUG_RETURN(true);
-	}
-
-	DBUG_RETURN(false);
-}
-
-
-static MY_ATTRIBUTE((nonnull, warn_unused_result))
-bool
-innobase_drop_foreigns_try(
-/*======================*/
-	trx_t*			trx,
-	const char*		table_name)
-{
-	DBUG_ENTER("innobase_drop_foreigns_try");
-
-	DBUG_ASSERT(trx_get_dict_operation(trx) == TRX_DICT_OP_INDEX);
-	ut_ad(trx->dict_operation_lock_mode == RW_X_LATCH);
-	ut_d(dict_sys.assert_locked());
-
-	/* Drop the constraint from the data dictionary. */
-	static const char sql[] =
-		"PROCEDURE DROP_FOREIGN_PROC () IS\n"
-		"BEGIN\n"
-		"DELETE FROM SYS_FOREIGN WHERE FOR_NAME=:table;\n"
-		"DELETE FROM SYS_FOREIGN_COLS WHERE ID=:id;\n"
-		"END;\n";
-
-	dberr_t		error;
-	pars_info_t*	info;
-
-	info = pars_info_create();
-	pars_info_add_str_literal(info, "table", table_name);
-
-	trx->op_info = "dropping foreign key constraints from dictionary";
 	error = que_eval_sql(info, sql, FALSE, trx);
 	trx->op_info = "";
 
